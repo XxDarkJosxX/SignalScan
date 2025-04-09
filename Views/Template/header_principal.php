638dd9b36ded11c27d2a37da41c5e9990bbbbc31
JoseM <maytadazajos@gmail.com>
2025-04-09 09:57:36 -0400

    v5

--- Views/Template/header_principal.php
+++ Views/Template/header_principal.php
@@ -47,7 +47,7 @@ https://templatemo.com/tm-570-chain-app-dev
     <link rel="stylesheet" href="Assets/css/animated.css">
     <link rel="stylesheet" href="Assets/css/owl.css">
     <link rel="stylesheet" href="Assets/css/nav.css">
-    <link rel="stylesheet" href="Assets/css/new.css?v=23">
+    <link rel="stylesheet" href="Assets/css/new.css?v=24">
 
   </head>
 
